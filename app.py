bbbc03d66d1b05d118752c8fe61db45e88efef4e
samolajide23 <d00231093@student.dkit.ie>
2022-09-14 16:40:00 +0100

    Moving The snake

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
+from string import whitespace
 import pygame
+
 pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.update()
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255, 255, 255)
+black = (0, 0, 0)
 
+dis = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Snake Game')
-blue = (0,0,255)
-red = (255,0,0)
-game_over =  False
+game_over = False
 
+x = 300
+y = 300
+x1 = 0
+y1 = 0
+clock = pygame.time.Clock()
 
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-    pygame.draw.rect(dis,blue,[200,150,10,10])
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1 = -10
+                y1 = 0
+            elif event.key == pygame.K_RIGHT:
+                x1 = 10
+                y1 = 0
+            elif event.key == pygame.K_UP:
+                y1 = -10
+                x1 = 0
+            elif event.key == pygame.K_DOWN:
+                y1 = 10
+                x1 = 0
+    x += x1
+    y += y1
+    dis.fill(black)
+    pygame.draw.rect(dis, blue, [x, y, 10, 10])
+
     pygame.display.update()
+
+    clock.tick(30)
 pygame.quit()
 quit()
